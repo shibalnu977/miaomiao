2b21d1c89be4e8a1fd374f4bb8d9151f97b3e1d5
shibalnu977 <14035901@qq.com>
2021-01-29 13:59:37 +0800

    add createComponents

---
+++ src/router/movie/index.js
@@ -0,0 +1,26 @@
+export default {
+    path :'/movie',
+    component:() => import('@/views/Movie'),
+    children : [
+        {
+            path : 'city',
+            component : () => import('@/components/City')
+        },
+        {
+            path : 'nowPlaying',
+            component : () => import('@/components/Nowplaying')
+        },
+        {
+            path : 'comingSoon',
+            component : () => import('@/components/Comingsoon')
+        },
+        {
+            path : 'search',
+            component : () => import('@/components/Search')
+        },
+        {
+            path : '/movie',
+            redirect : '/movie/nowPlaying'
+        }
+    ]
+}
